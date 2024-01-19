fa20a97366243ea1ac4a3116623d30c644c86fa5
MakEva <sher.evikm@gmail.com>
2024-01-19 13:34:58 +0200

    update

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,15 @@
+import css from './Filter.module.css';
+
+export const Filter = ({ changeFilter }) => {
+  return (
+    <label htmlFor="filter" className={css.filter}>
+      Find contacts by name
+      <input
+        onChange={changeFilter}
+        name="filter"
+        id="filter"
+        className={css.filter_input}
+      ></input>
+    </label>
+  );
+};
